c00a3423ee8a0e36b56f8c12d3c5d3a2988cd21c
Micah Stupak <micah@measureandfit.co>
2019-05-02 10:38:17 -0400

    updated diffenator

--- tools/diffenator.sh
+++ tools/diffenator.sh
@@ -3,7 +3,7 @@ echo "This script will give you the rendering output diffs."
 read -p 'Before file: ' before
 read -p 'After file: ' after
 read -p 'Output directory: ' output_dir
-read -p 'Threshold: ' thresh
+# read -p 'Threshold: ' thresh
 
 after_filename=$(basename "$after")
 
@@ -13,5 +13,5 @@ echo "Output to: $full_path"
 mkdir "$full_path"
 touch "$full_path/$after_filename".html
 
-diffenator "$before" "$after" --marks_thresh "$thresh" --mkmks_thresh "$thresh" --kerns_thresh "$thresh" --glyphs_thresh "$thresh" --metrics_thresh "$thresh" -rd -r "$full_path" -html > "$full_path/$after_filename".html
+diffenator "$before" "$after" --marks_thresh 10 --mkmks_thresh 10 --kerns_thresh 1 --glyphs_thresh 0.1 --metrics_thresh 1 -rd -r "$full_path" -html > "$full_path/$after_filename".html
 # mv output.html $after.html
