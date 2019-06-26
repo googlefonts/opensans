a2daca4834f2f844c2a0b0db8e6c5ae1a2c0bc09
Marc Foley <m.foley.88@gmail.com>
2019-06-26 15:02:03 +0100

    Run cu2qu on Roman and Italic fonts seperately

--- source/build.sh
+++ source/build.sh
@@ -7,15 +7,13 @@ fontmake -g ./OpenSans-Roman.glyphs -o ufo --no-production-names
 fontmake -g ./OpenSans-Italic.glyphs -o ufo --no-production-names
 
 echo "Converting cubic curves to quadratic"
-ufos=$(find *_ufo -name *.ufo -type d -maxdepth 1)
-for ufo in $ufos
-do
-	cu2qu -i $ufo
-done
+cu2qu ./master_ufo/OpenSans-Light.ufo/ ./master_ufo/OpenSans-Bold.ufo/ ./master_ufo/OpenSans-ExtraBold.ufo/ ./master_ufo/OpenSans-CondensedLight.ufo/ ./master_ufo/OpenSans-CondensedBold.ufo/ ./master_ufo/OpenSans-CondensedExtraBold.ufo/ -i
+cu2qu ./master_ufo/OpenSans-LightItalic.ufo/ ./master_ufo/OpenSans-ExtraBoldItalic.ufo/ ./master_ufo/OpenSans-CondensedLightItalic.ufo/ ./master_ufo/OpenSans-CondensedExtraBoldItalic.ufo/ -i
 
 echo "Generating VFs"
 fontmake -m OpenSans-Roman.designspace -o variable --output-path ../fonts/variable_ttf/OpenSans-Roman-VF.ttf
 fontmake -m OpenSans-Italic.designspace -o variable --output-path ../fonts/variable_ttf/OpenSans-Italic-VF.ttf
 
-rm -rf master_ufo/ instance_ufo/
+
+rm -rf ./master_ufo ./instance_ufo/
 
