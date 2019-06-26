e0112abb7cf40de8cd79b77e19ec0467329e1229
Micah Stupak <micah@measureandfit.co>
2019-06-26 14:29:34 -0400

    Revert "updated instantiator script"
    This reverts commit 1e3c5f5ce19305081010ef650622d42248da1712.

--- tools/instantiate.sh
+++ tools/instantiate.sh
@@ -1,5 +1,5 @@
 weights=(300 400 600 700 800)
-widths=(75 100)
+widths=(60 100)
 for I in ${weights[@]};
-	do python3 vf2s.py --width 100 --weight "$I" ../source/TTF\ VTT\ source/OpenSans-Roman.ttf
+	do python3 vf2s.py --width 100 --weight "$I" ../source/quadratic/variable_ttf/OpenSans-Roman-quadratic-VF.ttf
 done
