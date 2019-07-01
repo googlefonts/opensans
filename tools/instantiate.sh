14dfac4cb53307eef9f2e733461e534e43576a4c
Micah Stupak <micah@measureandfit.co>
2019-07-01 15:14:58 -0400

    new instantiation scripts using varLib

--- tools/instantiate.sh
+++
@@ -1,5 +0,0 @@
-weights=(300 400 600 700 800)
-widths=(60 100)
-for I in ${weights[@]};
-	do python3 vf2s.py --width 100 --weight "$I" ../source/quadratic/variable_ttf/OpenSans-Roman-quadratic-VF.ttf
-done
