a513345c0eef34c0e6b490c830dfbfe90b7b17de
Marc Foley <m.foley.88@gmail.com>
2019-07-29 16:39:54 +0100

    build.sh add vf filename variables

--- source/build.sh
+++ source/build.sh
@@ -11,8 +11,10 @@ cu2qu ./master_ufo/OpenSans-Light.ufo/ ./master_ufo/OpenSans-Bold.ufo/ ./master_
 cu2qu ./master_ufo/OpenSans-LightItalic.ufo/ ./master_ufo/OpenSans-ExtraBoldItalic.ufo/ ./master_ufo/OpenSans-CondensedLightItalic.ufo/ ./master_ufo/OpenSans-CondensedExtraBoldItalic.ufo/ -i
 
 echo "Generating VFs"
-fontmake -m OpenSans-Roman.designspace -o variable --output-path ../fonts/variable_ttf/OpenSans-Roman-VF.ttf
-fontmake -m OpenSans-Italic.designspace -o variable --output-path ../fonts/variable_ttf/OpenSans-Italic-VF.ttf
+VF_FILENAME_ROMAN="../fonts/variable_ttf/OpenSans-Roman-VF.ttf"
+VF_FILENAME_ITALIC="../fonts/variable_ttf/OpenSans-Italic-VF.ttf"
+fontmake -m OpenSans-Roman.designspace -o variable --output-path $VF_FILENAME_ROMAN
+fontmake -m OpenSans-Italic.designspace -o variable --output-path $VF_FILENAME_ITALIC
 
 
 rm -rf ./master_ufo ./instance_ufo/
