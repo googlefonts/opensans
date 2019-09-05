77e11c42e0a000c6cde6451ad5d1b3fe99ec89c1
Marc Foley <m.foley.88@gmail.com>
2019-09-05 16:23:32 +0100

    build:remove MVAR tables from VFs

--- source/build.sh
+++ source/build.sh
@@ -19,10 +19,20 @@ fontmake -m OpenSans-Italic.designspace -o variable --output-path $VF_FILENAME_I
 
 rm -rf ./master_ufo ./instance_ufo/
 
-# Patch name and stat tables
+# Drop MVAR and patch name and stat tables
 ttx -m "../fonts/variable_ttf/OpenSans-Roman-VF.ttf" OpenSans-Roman-patch.ttx
-mv OpenSans-Roman-patch.ttf "../fonts/variable_ttf/OpenSans-Roman-VF.ttf"
+mv OpenSans-Roman-patch.ttf $VF_FILENAME_ROMAN
+
+ttx -x "MVAR" $VF_FILENAME_ROMAN
+rm $VF_FILENAME_ROMAN
+ttx "${VF_FILENAME_ROMAN%.*}.ttx"
+
 
 ttx -m "../fonts/variable_ttf/OpenSans-Italic-VF.ttf" OpenSans-Italic-patch.ttx
 mv OpenSans-Italic-patch.ttf "../fonts/variable_ttf/OpenSans-Italic-VF.ttf"
 
+ttx -x "MVAR" $VF_FILENAME_ITALIC
+rm $VF_FILENAME_ITALIC
+ttx "${VF_FILENAME_ITALIC%.*}.ttx"
+
+rm ../fonts/variable_ttf/*.ttx
