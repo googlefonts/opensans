bb53341a98e905dfef15ccc87cb545d5f5847964
Denis Moyogo Jacquerye <moyogo@gmail.com>
2020-02-07 13:45:54 +0000

    Allow running build.sh anywhere

--- sources/build.sh
+++ sources/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# Go the sources directory to run commands
+SOURCE="${BASH_SOURCE[0]}"
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+cd $DIR
+echo $(pwd)
+
 rm -rf master_ufo/ instance_ufo/ instance_ufos/*
 
 echo "Generating Static fonts"
